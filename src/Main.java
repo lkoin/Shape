f61e1939ce71cb672d78d2e83ea2b6b5fb5d1e76
lkoin <lkoin.kontakt@gmail.com>
2023-09-22 09:09:40 +0200

    pelen kalkulator

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Square square = new Square();
-        Square square1 = new Square(5.0);
+
+        Square square = new Square(5.0);
         Circle circle = new Circle(3.0);
         Triangle triangle = new Triangle(3.0,5.0);
         Scanner scanner = new Scanner(System.in);
@@ -17,15 +17,34 @@ public class Main {
         int number = scanner.nextInt();
         switch (number) {
             case 1:
-                System.out.println("Pole kwadratu wynosi: \n" + square1.calculateArea());
+                System.out.println("Podaj dlugosc boku kwadratu: ");
+                square.side = scanner.nextDouble();
+                System.out.println("Pole kwadratu wynosi: \n" + square.calculateArea());
                 break;
             case 2:
+                System.out.println("Podaj dlugosc promienia: ");
+                circle.radius= scanner.nextDouble();
                 System.out.printf("Pole kola o promeniu %.2f wynosi : \n %.2f", circle.radius, circle.calculateCircuit());
                 break;
             case 3:
-                System.out.println("Pole kwadratu wynosi: \n" + triangle.trangleArea());
-                System.out.printf("Dlugosc boku c wynosi: %.2f", triangle.triangleCircuit());
+                System.out.println("1. Oblicz pole");
+                System.out.println("2. Oblicz bok c");
+                int wybor = scanner.nextInt();
+                if (wybor== 1) {
+                    System.out.println("Podaj dlugosc boku a trojkąta: ");
+                    triangle.boka = scanner.nextDouble();
+                    System.out.println("Podaj dlugosc boku b trojkąta: ");
+                    triangle.bokb = scanner.nextDouble();
+                    System.out.println("Pole kwadratu wynosi: \n" + triangle.trangleArea());
+                } else if (wybor==2) {
+                    System.out.println("Podaj dlugosc boku a trojkąta: ");
+                    triangle.boka = scanner.nextDouble();
+                    System.out.println("Podaj dlugosc boku b trojkąta: ");
+                    triangle.bokb = scanner.nextDouble();
+                    System.out.printf("Dlugosc boku c wynosi: %.2f", triangle.triangleCircuit());
+                }
                 break;
+
             case 0:
                 System.out.println("Wyjscie z programu");
                 petla = false;
